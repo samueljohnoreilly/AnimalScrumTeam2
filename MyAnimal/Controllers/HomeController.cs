916dc80ea510514f4512c0c112968b24cb2f84aa
Sarah Howells <sarah.howells@method4.co.uk>
2020-03-19 15:07:25 +0000

    communications

--- MyAnimal/Controllers/HomeController.cs
+++ MyAnimal/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace MyAnimal.Controllers {
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Communications() {
+	        return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
